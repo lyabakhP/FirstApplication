e46fc20d533a5f9d64dc838cf6dc3e5abb251032
Paul <lyabakhp@gmail.com>
2016-05-25 19:22:23 +0300

    FirstTest

--- src/com/company/ArrayServiceTest.java
+++ src/com/company/ArrayServiceTest.java
@@ -10,13 +10,23 @@ import static org.junit.Assert.*;
  */
 public class ArrayServiceTest {
     @Test
+    public void reverse() throws Exception {
+        ArrayService arrayService = new ArrayService();
+        int [] arrayPavel = {1,2,3,4,5};
+        arrayService.reverse(arrayPavel);
+        int [] expectedPavel = {5,4,3,2,1};
+        int [] actualPavel = arrayPavel;
+        Assert.assertArrayEquals(expectedPavel,actualPavel);
+    }
+
+    @Test
     public void reversNew() throws Exception {
-ArrayService arrayService = new ArrayService();
-        int [] arr = {1,2,3,4,5};
-        arrayService.reversNew(arr);
-        int [] expected = {5,4,3,2,1};
-        int [] actual = arr;
-        Assert.assertArrayEquals(expected,actual);
+        ArrayService arrayService = new ArrayService();
+        int [] arrAlexander = {1,2,3,4,5};
+        arrayService.reversNew(arrAlexander);
+        int [] expectedAlexander = {5,4,3,2,1};
+        int [] actualAlexander = arrAlexander;
+        Assert.assertArrayEquals(expectedAlexander,actualAlexander);
     }
 
 }
