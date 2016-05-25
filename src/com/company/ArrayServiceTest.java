2b1fd58934de76bb2fbbb50d4691730c6b5a052b
Paul <lyabakhp@gmail.com>
2016-05-25 17:50:03 +0300

    return [I@1540e19d

---
+++ src/com/company/ArrayServiceTest.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by anazema on 25.05.2016.
+ */
+public class ArrayServiceTest {
+    @Test
+    public void reversNew() throws Exception {
+ArrayService arrayService = new ArrayService();
+        //arrayService.reversNew();
+    }
+
+}
