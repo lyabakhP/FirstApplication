d625cd82ac2a370c6c5e244568f5c3b3c91332f5
Paul <lyabakhp@gmail.com>
2016-05-25 19:23:22 +0300

    twoMethods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int [] num = {1,8,9,4,7};
+        int[] num = {1, 8, 9, 4, 7};
         ArrayService arrayService = new ArrayService();
 
         arrayService.print(num);
-        arrayService.reversNew(num);
+
+        arrayService.reverse(num);
         arrayService.print(num);
 
+        arrayService.reversNew(num);
+        arrayService.print(num);
     }
 }
